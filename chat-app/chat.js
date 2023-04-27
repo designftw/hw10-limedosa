e999f365c3369393891d6b51d2eb8cac7802920c
limedosa <109569567+limedosa@users.noreply.github.com>
2023-04-26 22:31:28 -0400

    h

--- chat-app/chat.js
+++ chat-app/chat.js
@@ -1,7 +1,7 @@
-import * as Vue from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+import * as Vue from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 import { mixin } from "https://mavue.mavo.io/mavue.js";
-import GraffitiPlugin from 'https://graffiti.garden/graffiti-js/plugins/vue/plugin.js'
-import Resolver from './resolver.js'
+import GraffitiPlugin from "https://graffiti.garden/graffiti-js/plugins/vue/plugin.js";
+import Resolver from "./resolver.js";
 
 const app = {
   // Import MaVue
@@ -9,185 +9,274 @@ const app = {
 
   // Import resolver
   created() {
-    this.resolver = new Resolver(this.$gf)
+    this.resolver = new Resolver(this.$gf);
   },
 
   setup() {
     // Initialize the name of the channel we're chatting in
-    const channel = Vue.ref('default')
+    const channel = Vue.ref("default");
 
     // And a flag for whether or not we're private-messaging
-    const privateMessaging = Vue.ref(false)
+    const privateMessaging = Vue.ref(false);
 
     // If we're private messaging use "me" as the channel,
     // otherwise use the channel value
-    const $gf = Vue.inject('graffiti')
-    const context = Vue.computed(()=> privateMessaging.value? [$gf.me] : [channel.value])
+    const $gf = Vue.inject("graffiti");
+    const context = Vue.computed(() =>
+      privateMessaging.value ? [$gf.me] : [channel.value]
+    );
 
     // Initialize the collection of messages associated with the context
-    const { objects: messagesRaw } = $gf.useObjects(context)
-    return { channel, privateMessaging, messagesRaw }
+    const { objects: messagesRaw } = $gf.useObjects(context);
+    return { channel, privateMessaging, messagesRaw };
   },
 
   data() {
     // Initialize some more reactive variables
     return {
-      messageText: '',
-      editID: '',
-      editText: '',
-      recipient: ''
-    }
+      recipientUsername: "",
+      messageText: "",
+      editID: "",
+      editText: "",
+      recipient: "",
+      sortOrder: "newestFirst",
+      preferredUsername: "",
+      usernameResult: "",
+
+      recipientUsernameSearch: "",
+    };
   },
 
   computed: {
+    populateUsername(actor, username) {},
     messages() {
       let messages = this.messagesRaw
         // Filter the "raw" messages for data
         // that is appropriate for our application
         // https://www.w3.org/TR/activitystreams-vocabulary/#dfn-note
-        .filter(m=>
-          // Does the message have a type property?
-          m.type         &&
-          // Is the value of that property 'Note'?
-          m.type=='Note' &&
-          // Does the message have a content property?
-          m.content      &&
-          // Is that property a string?
-          typeof m.content=='string') 
+        .filter(
+          (m) =>
+            // Does the message have a type property?
+            m.type &&
+            // Is the value of that property 'Note'?
+            m.type == "Note" &&
+            // Does the message have a content property?
+            m.content &&
+            // Is that property a string?
+            typeof m.content == "string"
+        );
 
       // Do some more filtering for private messaging
       if (this.privateMessaging) {
-        messages = messages.filter(m=>
-          // Is the message private?
-          m.bto &&
-          // Is the message to exactly one person?
-          m.bto.length == 1 &&
-          (
+        messages = messages.filter(
+          (m) =>
+            // Is the message private?
+            m.bto &&
+            // Is the message to exactly one person?
+            m.bto.length == 1 &&
             // Is the message to the recipient?
-            m.bto[0] == this.recipient ||
-            // Or is the message from the recipient?
-            m.actor == this.recipient
-          ))
+            (m.bto[0] == this.recipient ||
+              // Or is the message from the recipient?
+              m.actor == this.recipient)
+        );
       }
 
-      return messages
-        // Sort the messages with the
-        // most recently created ones first
-        .sort((m1, m2)=> new Date(m2.published) - new Date(m1.published))
-        // Only show the 10 most recent ones
-        .slice(0,10)
+      return (
+        messages
+          // Sort the messages with the
+          // most recently created ones first
+          .sort((m1, m2) => new Date(m2.published) - new Date(m1.published))
+          // Only show the 10 most recent ones
+          .slice(0, 10)
+      );
     },
   },
 
   methods: {
+    async searchByUsername() {
+      this.loading = true;
+      const actor = await this.resolver.usernameToActor(this.username);
+      this.loading = false;
+      if (actor) {
+        this.recipient = actor;
+      } else {
+        alert("Username not found!");
+      }
+    },
+    lookupUsername(actor) {
+      this.resolver.actorToUsername(actor).then((response) => {
+        let username = response || "No Username"; // if no username, display this
+        let allMessages = document.getElementsByClassName("username");
+        if (allMessages) {
+          for (let i = 0; i < allMessages.length; i++) {
+            if (allMessages[i].innerHTML == actor) {
+              allMessages[i].innerHTML = username;
+            }
+          }
+        }
+        return username; // Return the resolved username (or "No Username") here
+      });
+      // this.resolver.actorToUsername(actor).then((response) => {
+      //   let username = response || actor; // Use response if it exists, otherwise use actor
+      //   let allMessages = document.getElementsByClassName("username");
+      //   if (allMessages) {
+      //     for (let i = 0; i < allMessages.length; i++) {
+      //       if (allMessages[i].innerHTML == actor) {
+      //         allMessages[i].innerHTML = username;
+      //       }
+      //     }
+      //   }
+      // });
+      // this.resolver.actorToUsername(actor).then((response) => {
+      //   let username = response || actor; // Use response if it exists, otherwise use actor
+      //   let allMessages = document.getElementsByClassName("username");
+      //   if (allMessages) {
+      //     for (let i = 0; i < allMessages.length; i++) {
+      //       if (allMessages[i].innerHTML == actor) {
+      //         allMessages[i].innerHTML = username;
+      //       }
+      //     }
+      //   }
+      //   return username; // Return the resolved username (or actor name) here
+      // });
+
+      // this.resolver.actorToUsername(actor).then((response) => {
+      //   if (response) {
+      //     console.log(response);
+      //     let allMessages = document.getElementsByClassName("username");
+      //     // console.log(allMessages.length);
+      //     if (allMessages) {
+      //       for (let i = 0; i < allMessages.length; i++) {
+      //         console.log(i);
+      //         console.log(allMessages[i].innerHTML);
+      //         if (allMessages[i].innerHTML == actor) {
+      //           allMessages[i].innerHTML = response;
+      //         }
+      //         // allMessages[i].style.color = "purple";
+      //         // let thisActorID = allMessages[i].closest(".message .actor").innerHTML;
+      //         // allMessages[i].innerHTML = thisActorID;
+      //         // console.log(thisActorID);
+      //       }
+      //     }
+      //   }
+      // });
+
+      return actor;
+    },
     sendMessage() {
       const message = {
-        type: 'Note',
+        type: "Note",
         content: this.messageText,
-      }
+      };
 
       // The context field declares which
       // channel(s) the object is posted in
       // You can post in more than one if you want!
       // The bto field makes messages private
       if (this.privateMessaging) {
-        message.bto = [this.recipient]
-        message.context = [this.$gf.me, this.recipient]
+        message.bto = [this.recipient];
+        message.context = [this.$gf.me, this.recipient];
       } else {
-        message.context = [this.channel]
+        message.context = [this.channel];
       }
 
       // Send!
-      this.$gf.post(message)
+      this.$gf.post(message);
     },
 
     removeMessage(message) {
-      this.$gf.remove(message)
+      this.$gf.remove(message);
     },
 
     startEditMessage(message) {
       // Mark which message we're editing
-      this.editID = message.id
+      this.editID = message.id;
       // And copy over it's existing text
-      this.editText = message.content
+      this.editText = message.content;
     },
 
     saveEditMessage(message) {
       // Save the text (which will automatically
       // sync with the server)
-      message.content = this.editText
+      message.content = this.editText;
       // And clear the edit mark
-      this.editID = ''
-    }
-  }
-}
+      this.editID = "";
+    },
+  },
+};
 
 const Name = {
-  props: ['actor', 'editable'],
+  props: ["actor", "editable"],
 
   setup(props) {
     // Get a collection of all objects associated with the actor
-    const { actor } = Vue.toRefs(props)
-    const $gf = Vue.inject('graffiti')
-    return $gf.useObjects([actor])
+    const { actor } = Vue.toRefs(props);
+    const $gf = Vue.inject("graffiti");
+    return $gf.useObjects([actor]);
   },
 
   computed: {
     profile() {
-      return this.objects
-        // Filter the raw objects for profile data
-        // https://www.w3.org/TR/activitystreams-vocabulary/#dfn-profile
-        .filter(m=>
-          // Does the message have a type property?
-          m.type &&
-          // Is the value of that property 'Profile'?
-          m.type=='Profile' &&
-          // Does the message have a name property?
-          m.name &&
-          // Is that property a string?
-          typeof m.name=='string')
-        // Choose the most recent one or null if none exists
-        .reduce((prev, curr)=> !prev || curr.published > prev.published? curr : prev, null)
-    }
+      return (
+        this.objects
+          // Filter the raw objects for profile data
+          // https://www.w3.org/TR/activitystreams-vocabulary/#dfn-profile
+          .filter(
+            (m) =>
+              // Does the message have a type property?
+              m.type &&
+              // Is the value of that property 'Profile'?
+              m.type == "Profile" &&
+              // Does the message have a name property?
+              m.name &&
+              // Is that property a string?
+              typeof m.name == "string"
+          )
+          // Choose the most recent one or null if none exists
+          .reduce(
+            (prev, curr) =>
+              !prev || curr.published > prev.published ? curr : prev,
+            null
+          )
+      );
+    },
   },
 
   data() {
     return {
       editing: false,
-      editText: ''
-    }
+      editText: "",
+    };
   },
 
   methods: {
     editName() {
-      this.editing = true
+      this.editing = true;
       // If we already have a profile,
       // initialize the edit text to our existing name
-      this.editText = this.profile? this.profile.name : this.editText
+      this.editText = this.profile ? this.profile.name : this.editText;
     },
 
     saveName() {
       if (this.profile) {
         // If we already have a profile, just change the name
         // (this will sync automatically)
-        this.profile.name = this.editText
+        this.profile.name = this.editText;
       } else {
         // Otherwise create a profile
         this.$gf.post({
-          type: 'Profile',
-          name: this.editText
-        })
+          type: "Profile",
+          name: this.editText,
+        });
       }
 
       // Exit the editing state
-      this.editing = false
-    }
+      this.editing = false;
+    },
   },
 
-  template: '#name'
-}
+  template: "#name",
+};
 
-app.components = { Name }
-Vue.createApp(app)
-   .use(GraffitiPlugin(Vue))
-   .mount('#app')
+app.components = { Name };
+Vue.createApp(app).use(GraffitiPlugin(Vue)).mount("#app");
--- chat-app/resolver.js
+++ chat-app/resolver.js
@@ -1,45 +1,60 @@
 export default class UsernameResolver {
-
-  constructor(graffiti, context=['designftw.mit.edu']) {
-    this.gf = graffiti
-    this.context = context
+  constructor(graffiti, context = ["designftw.mit.edu"]) {
+    this.gf = graffiti;
+    this.context = context;
   }
 
-  async usernameToActor(username, signal=AbortSignal.timeout(500)) {
-    const offer = await this.#resolveOffer(object=> 
-      object.object.preferredUsername == username, signal)
-    return offer? offer.actor : null
+  async usernameToActor(username, signal = AbortSignal.timeout(500)) {
+    const offer = await this.#resolveOffer(
+      (object) => object.object.preferredUsername == username,
+      signal
+    );
+    return offer ? offer.actor : null;
   }
 
-  async actorToUsername(actor, signal=AbortSignal.timeout(500)) {
-    const offer = await this.#resolveOffer(object=> 
-      object.actor == actor &&
-      object.object.actor == actor, signal)
-    return offer? offer.object.preferredUsername : null
+  async actorToUsername(actor, signal = AbortSignal.timeout(500)) {
+    const offer = await this.#resolveOffer(
+      (object) => object.actor == actor && object.object.actor == actor,
+      signal
+    );
+    return offer ? offer.object.preferredUsername : null;
+  }
+  async sendMessage() {
+    const content = this.messageText.trim();
+    if (!content) return;
+
+    const bto = this.recipient
+      ? [await resolver.resolveActor(this.recipient)]
+      : [];
+    const message = await $gf.create({ "@type": "Note", content, bto });
+
+    this.messageText = "";
+    this.recipient = "";
   }
 
-  async requestUsername(preferredUsername, signal=AbortSignal.timeout(500)) {
+  async requestUsername(preferredUsername, signal = AbortSignal.timeout(500)) {
     // Check if we have already have made an offer
-    let offer = null
+    let offer = null;
     for await (const object of this.gf.objects(this.context, signal)) {
       if (this.#offerFilter(object)) {
-
         // Someone else already has the name!
-        if (object.actor != this.gf.me &&
-            object.object.preferredUsername == preferredUsername) {
-          throw "Username already claimed!"
+        if (
+          object.actor != this.gf.me &&
+          object.object.preferredUsername == preferredUsername
+        ) {
+          throw "ERROR! Username's already taken. ";
         }
 
         // An offer already exists!
         if (object.actor == this.gf.me) {
           if (!offer) {
-            offer = object
+            offer = object;
           } else if (object.updated < offer.updated) {
             // Remove hanging offers if they exist
-            this.gf.remove(offer)
-            offer = object
+            this.gf.remove(offer);
+            offer = object;
           } else {
-            this.gf.remove(object)
+            this.gf.remove(object);
           }
         }
       }
@@ -47,42 +62,53 @@ export default class UsernameResolver {
 
     // If an offer already exists, just change it
     if (offer) {
-      offer.object.preferredUsername = preferredUsername
-      return "success"
+      offer.object.preferredUsername = preferredUsername;
+      // return "Success! Your new username is set." ;
+      // alert ("SUCCESS! Username Changed.");
+      // return `Your new username: ${preferredUsername}`;
+      var result = `Your new username is: ${preferredUsername}`;
+      // return "Success! Your new username is set.";
+      alert("SUCCESS! Username Changed. "+result);
+      // alert("Hello, world!");
+      return result;
     }
 
     // Otherwise make a new one
     this.gf.post({
-      type: 'Offer',
+      type: "Offer",
       object: {
-        type: 'Profile',
+        type: "Profile",
         preferredUsername,
-        actor: this.gf.me
+        actor: this.gf.me,
       },
       target: {
-        type: 'NameSystem',
-        namespace: this.context
+        type: "NameSystem",
+        namespace: this.context,
       },
-      context: this.context
-    })
-    return "success"
+      context: this.context,
+    });
+
+    var result = `Your new username is: ${preferredUsername}`;
+    // return "Success! Your new username is set.";
+    alert("SUCCESS! Username Changed. "+result);
+    return result;
   }
 
   async #resolveOffer(condition, signal) {
-    let offer = null
+    let offer = null;
     for await (const object of this.gf.objects(this.context, signal)) {
       if (this.#offerFilter(object) && condition(object)) {
         if (!offer) {
-          offer = object
+          offer = object;
         } else if (object.updated < offer.updated) {
-          offer = object
+          offer = object;
         }
       }
     }
 
     // If there is nothing accepted
     // just return an arbitrary offer if one exists
-    return offer
+    return offer;
   }
 
   // The target of offers and requests
@@ -91,11 +117,13 @@ export default class UsernameResolver {
   //    namespace: ['mycontext', ...]
   // }
   #targetFilter(t) {
-    return t.type &&
-           t.type == 'NameSystem' &&
-           t.namespace &&
-           Array.isArray(t.namespace) &&
-           t.namespace.includes(this.context[0])
+    return (
+      t.type &&
+      t.type == "NameSystem" &&
+      t.namespace &&
+      Array.isArray(t.namespace) &&
+      t.namespace.includes(this.context[0])
+    );
   }
 
   // The object of offers and requests
@@ -105,14 +133,17 @@ export default class UsernameResolver {
   //    actor: 'graffitiactor://xyz'
   // }
   #objectFilter(o) {
-    return o.actor &&
-           typeof o.actor == 'string' &&
-           o.type &&
-           o.type == 'Profile' &&
-           o.preferredUsername &&
-           typeof o.preferredUsername == 'string'
+    return (
+      o.actor &&
+      typeof o.actor == "string" &&
+      o.type &&
+      o.type == "Profile" &&
+      o.preferredUsername &&
+      typeof o.preferredUsername == "string"
+    );
   }
 
+  //
   // {
   //    type: 'Offer'
   //    target: {
@@ -125,11 +156,13 @@ export default class UsernameResolver {
   //    }
   // }
   #offerFilter(o) {
-    return o.type &&
-           o.type == 'Offer' &&
-           o.target &&
-           this.#targetFilter(o.target) &&
-           o.object &&
-           this.#objectFilter(o.object)
+    return (
+      o.type &&
+      o.type == "Offer" &&
+      o.target &&
+      this.#targetFilter(o.target) &&
+      o.object &&
+      this.#objectFilter(o.object)
+    );
   }
 }
